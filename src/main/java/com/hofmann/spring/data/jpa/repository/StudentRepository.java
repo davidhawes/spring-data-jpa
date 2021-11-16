ff4974fd0d3d6a5c61cf42f12b4ef769ae193839
davidhawes <dhofmann@tsn.at>
2021-11-16 19:18:12 +0100

    Repository und Repository test

---
+++ src/main/java/com/hofmann/spring/data/jpa/repository/StudentRepository.java
@@ -0,0 +1,9 @@
+package com.hofmann.spring.data.jpa.repository;
+
+import com.hofmann.spring.data.jpa.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
+}
---
+++ src/test/java/com/hofmann/spring/data/jpa/repository/StudentRepositoryTest.java
@@ -0,0 +1,38 @@
+package com.hofmann.spring.data.jpa.repository;
+
+import com.hofmann.spring.data.jpa.entity.Student;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class StudentRepositoryTest {
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @Test
+    public void saveStudent(){
+        Student student = Student.builder()
+                .emailId("david@gmail.com").
+                firstName("David")
+                .lastName("Hofmann")
+                .guardianName("guardian1")
+                .guardianEmail("guardian1@gmail.com")
+                .guardianMobile("9999999999")
+                .build();
+
+        studentRepository.save(student);
+    }
+
+    @Test
+    public void printAllStudent(){
+        List<Student> studentList = studentRepository.findAll();
+        System.out.println("studentList = " + studentList);
+    }
+
+}
