b7e5b855f872ab2b61492ccae5ca878b0e7be582
davidhawes <dhofmann@tsn.at>
2021-11-16 21:51:23 +0100

    CourseRepository

---
+++ src/main/java/com/hofmann/spring/data/jpa/repository/CourseRepository.java
@@ -0,0 +1,9 @@
+package com.hofmann.spring.data.jpa.repository;
+
+import com.hofmann.spring.data.jpa.entity.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CourseRepository extends JpaRepository<Course, Long> {
+}
